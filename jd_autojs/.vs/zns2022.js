d1b09b72de8eb7a1a600fba28066a971dfbd362e
Hao <zhang_zhi_hao@foxmail.com>
2022-01-19 18:24:28 +0800

    2022/1/19更新

--- jd_autojs/.vs/zns2022.js
+++ jd_autojs/.vs/zns2022.js
@@ -1,8 +1,7 @@
+//京东app名
 var appName = "com.jingdong.app.mall";
-//var huodong_indexInParent_num = 9;
-//var headerXY;
 // 需要完成的任务列表
-var TASK_LIST = ["早起", "城城", "浏览领", "浏览并关注8s", "浏览8秒", "浏览8s", "累计浏览", "浏览并关注可得2000", "浏览可得", "入会", "浏览并关注可得", "去首页浮层进入", "浏览5个品牌墙店铺", "小程序", "浏览5个品牌墙店铺", "浏览活动"];
+var TASK_LIST = ["浏览领", "浏览并关注8s", "城城", "浏览8秒", "浏览8s", "累计浏览", "浏览并关注可得2000", "浏览可得", "入会", "浏览并关注可得", "去首页浮层进入", "小程序", "浏览5个品牌墙店铺", "浏览活动", "品牌墙店铺", "逛晚会页", "玩AR游戏可得", "去组队可得"];
 // 过渡操作
 var PASS_LIST = ['请选择要使用的应用', '我知道了', '取消', "京口令已复制"];
 // 判断停留时间
@@ -17,11 +16,12 @@ var current_task_num = 0;
 var isBackFlag = false;
 // 小程序标记
 var isXcx = false;
-//var appName = "com.jingdong.app.mall";
+// 活动按钮indexInParent值
 var huodong_indexInParent_num = 9;
 // 记录活动页面头部坐标
 var headerXY;
-var AppType = 0; //1: 京东APP  2:京东金融
+
+
 /**
  * 启动京东app
  */
@@ -31,7 +31,7 @@ function start()
     if (launch(appName)) 
     {
         console.info("作者:Cool-baby");
-        console.info("时间:2022-1-15");
+        console.info("时间:2022-1-19");
         console.info("GitHub:https://github.com/Cool-baby")
         console.info("启动京东APP");
     }
@@ -53,7 +53,11 @@ sleep(3000);
     var flag = getNeedSelector();//找任务
 
     if(viewTask(flag) == 0)
-        viewProduct();
+        if(viewProduct())
+        {
+            console.info("我滴任务完成啦！");
+            //break;
+        }
 }
 
 
@@ -149,37 +153,45 @@ function enterActivity()
  * 获取需要进行的控件
  * @returns
  */
-function getNeedSelector() {
+function getNeedSelector() 
+{
     var allSelector = className('android.view.View')
     .depth(19)
     .indexInParent(3)
     .drawingOrder(0)
     .clickable()
     .find();
-
-for (let index = 0; index < allSelector.length; index++) {
-    for (var i = 0; i < TASK_LIST.length; i++) {
+//console.info("allselectos: ",allSelector)
+for (let index = 0; index < allSelector.length; index++) 
+{
+    //console.info("index: ",index)
+    for (var i = 0; i < TASK_LIST.length; i++) 
+    {
         // 获取具有需要完成任务字符串的控件集合
         var list = allSelector[index].parent().findByText(TASK_LIST[i]);
+        //console.info("list: ",list)
         // 如果长度大于0则表示存在该控件
-        if (list.size() > 0) {
+        if (list.size() > 0) 
+        {
             // 获取不在序列中的序号
-            if (finished_task_num.indexOf(index) < 0) {
+            if (finished_task_num.indexOf(index) < 0) 
+            {
                 console.info("当前已完成序列：", finished_task_num)
                 current_task_num = index;
-            } else {
+            } 
+            else 
+            {
                 continue;
             }
 
             // 如果是浏览就返回的任务，将标记设为true
-            isBackFlag = (TASK_LIST[i].indexOf("浏览可得") >= 0 || TASK_LIST[i].indexOf("浏览并关注可得2000") >= 0 || TASK_LIST[i].indexOf("玩AR游戏可得") >= 0) ? true : false;
+            isBackFlag = (TASK_LIST[i].indexOf("浏览可得") >= 0 || TASK_LIST[i].indexOf("浏览并关注可得2000") >= 0 || TASK_LIST[i].indexOf("玩AR游戏可得") >= 0 || TASK_LIST[i].indexOf("去组队可得") >= 0) ? true : false;
             // 如果是小程序任务，将小程序标记设为true
             isXcx = (TASK_LIST[i].indexOf("小程序") >= 0) ? true : false;
             var rect = allSelector[current_task_num].bounds();
             if (text("累计任务奖励").exists()) {
                 console.info("去完成任务，当前任务序列：", current_task_num)
                 randomClick(rect.centerX(), rect.centerY());
-                //console.info("开始任务:", allSelector[current_task_num].parent().findByText(TASK_LIST[i]).get(0).text());
                 return true;
             }
         }
@@ -192,7 +204,8 @@ for (let index = 0; index < allSelector.length; index++) {
  * 去完成任务
  * [url=home.php?mod=space&uid=952169]@Param[/url] {是否点击任务标识} flag
  */
- function viewTask(flag) {
+ function viewTask(flag) 
+ {
     var res = 0;
     // 根据坐标点击任务，判断哪些需要进行
     sleep(2000);
@@ -210,46 +223,25 @@ for (let index = 0; index < allSelector.length; index++) {
          
         if (text("天天都能领").exists() || text("有机会得大额现金").exists() || text("邀请新朋友 更快赚现金").exists()) {
             console.info("进入城城领现金");
-            if(text("天天都能领").exists()) {
-                var btn = textContains('天天都能领').findOne().parent().children()[3].bounds()
-                click(btn.centerX(),btn.centerY())
-                sleep(1000);
-                log("给钱就收");
-            }
-            if(text("邀请新朋友 更快赚现金").exists()) {
-                var btn = className("android.view.View").text("邀请新朋友 更快赚现金").findOne().parent().children()[2].bounds()
-                click(btn.centerX(),btn.centerY())
-                sleep(1000);
-                log("点掉浮窗");
-            }
-            sleep(1000);
-            if( textContains("有机会得大额现金").exists()) {
-                var btn = textContains('有机会得大额现金').findOne().parent().children()[1].bounds()
-                click(btn.centerX(),btn.centerY())
-                sleep(1000);
-                log("城城邀请");
-            }
- 
-            if( textContains("去微信粘贴给好友").exists()){
-                var btn = textContains('去微信粘贴给好友').findOne().bounds()
-                click(btn.centerX(),btn.centerY())
-                sleep(1000);
-                log("关闭邀请码");
-            }
- 
+            console.info("城城分现金已结束");
+            // 将当前任务序号添加到列表中，防止后续点到
+            finished_task_num[finished_task_num.length] = current_task_num;
             viewAndFollow();
             sleep(500);
             // 重置计时
             JUDGE_TIME = 0;
-        }else
-        if ((textStartsWith("获得").exists() && textEndsWith("爆竹").exists()) || text("已浏览").exists() || text("浏览完成").exists()) {
+        }
+        else if ((textStartsWith("获得").exists() && textEndsWith("爆竹").exists()) || text("已浏览").exists() || text("浏览完成").exists()) 
+        {
             console.info("任务完成，返回");
             viewAndFollow();
             sleep(500);
             // 重置计时
             JUDGE_TIME = 0;
             break;
-        } else if (text("已达上限").exists()) {
+        } 
+        else if (text("已达上限").exists()) 
+        {
             console.info("任务已达上限,切换已完成按钮");
             // 将当前任务序号添加到列表中，防止后续点到
             finished_task_num[finished_task_num.length] = current_task_num;
@@ -259,13 +251,16 @@ for (let index = 0; index < allSelector.length; index++) {
             JUDGE_TIME = 0;
             res = 1;
             break;
-        }  else if (textContains('当前页点击浏览4个').exists() || textContains('当前页浏览加购').exists()
-        || textContains('当前页点击浏览4个商品领爆竹').exists() || textContains('当前页浏览加购4个商品领爆竹').exists()) {
+        }  
+        else if (textContains('当前页点击浏览4个').exists() || textContains('当前页浏览加购').exists() || textContains('当前页点击浏览4个商品领爆竹').exists() || textContains('当前页浏览加购4个商品领爆竹').exists()) 
+        {
         console.info("当前为加入购物车任务或浏览商品任务");
         // 重置计时
         JUDGE_TIME = 0;
         break;
-        } else if (textContains('会员授权协议').exists()) {
+        } 
+        else if (textContains('会员授权协议').exists()) 
+        {
             console.info("不加会员，切换已完成");
             // 将当前任务序号添加到列表中，防止后续点到
             finished_task_num[finished_task_num.length] = current_task_num;
@@ -273,17 +268,31 @@ for (let index = 0; index < allSelector.length; index++) {
             // 重置计时
             JUDGE_TIME = 0;
             break;
-        } else if (text("互动种草城").exists()) {
+        } 
+        else if (textContains('欢迎下次再来~').exists()) 
+        {
+            console.info("城城分现金已结束，切换已完成");
+            // 将当前任务序号添加到列表中，防止后续点到
+            finished_task_num[finished_task_num.length] = current_task_num;
+            viewAndFollow();
+            // 重置计时
+            JUDGE_TIME = 0;
+            break;
+        }
+        else if (text("互动种草城").exists()) 
+        {
             console.info("当前为互动种草城任务");
             // 重置计时
             JUDGE_TIME = 0;
-            if (interactionGrassPlanting()) {
+            if (interactionGrassPlanting()) 
+            {
                 back();
                 break;
             }
             break;
-        } else if (text("到底了，没有更多了～").exists() && !text("消息").exists() && !text("扫啊扫").exists()
-            && !(textStartsWith("当前进度").exists() && textEndsWith("10").exists())) {
+        } 
+        else if (text("到底了，没有更多了～").exists() && !text("消息").exists() && !text("扫啊扫").exists() && !(textStartsWith("当前进度").exists() && textEndsWith("10").exists())) 
+        {
             console.info("到底了，没有更多了～");
             sleep(1000);
             // 重置计时
@@ -317,10 +326,14 @@ for (let index = 0; index < allSelector.length; index++) {
             swipe(807, 314, 807, 414, 1);
             sleep(2000);
             break;
-        } else if (text("消息").exists() && text("扫啊扫").exists()) {
+        } 
+        else if (text("消息").exists() && text("扫啊扫").exists()) 
+        {
             console.warn("因为某些原因回到首页，重新进入活动界面");
             enterActivity();
-        } else if (text("天天都能领").exists()) {
+        } 
+        else if (text("天天都能领").exists()) 
+        {
             sleep(2000);
             console.info("天天都能领");
             // 重置计时
@@ -337,16 +350,17 @@ for (let index = 0; index < allSelector.length; index++) {
                     break;
                 }
             }
-        } else if (text('京东11.11热爱环...').exists()) {
-            console.info("下单任务，跳过");
-            back();
-        } else if (isBackFlag) {
+        } 
+        else if (isBackFlag) 
+        {
             console.info("进入浏览就返回任务");
             //sleep(1000);
             viewAndFollow();
             isBackFlag = false;
             break;
-        } else if (isXcx) {
+        } 
+        else if (isXcx) 
+        {
             console.info("进入小程序任务");
             // 重置计时
             JUDGE_TIME = 0;
@@ -368,14 +382,17 @@ for (let index = 0; index < allSelector.length; index++) {
  
             isXcx = false;
             break;
-        } else {
-            if (recoverApp()) {
+        } 
+        else 
+        {
+            if (recoverApp()) 
+            {
                 break;
             }
         }
     }
- 
-    if ((timenow - timestart) > (timeout * 1000)) {
+    if ((timenow - timestart) > (timeout * 1000)) 
+    {
         console.info("界面超时了")
         viewAndFollow();
     }
@@ -384,30 +401,42 @@ for (let index = 0; index < allSelector.length; index++) {
 
 
 /**
- * 浏览商品+加购商品
+ * 浏览商品/加购商品
+ * 实现效果类似，固只需一个方法
  */
- function viewProduct() {
-    if (textContains('当前页点击浏览4个商品领爆竹').exists() || textContains('当前页浏览加购4个商品领爆竹').exists()) {
+ function viewProduct() 
+ {
+    var check_flag = 1;
+    if (textContains('当前页点击浏览4个商品领爆竹').exists() || textContains('当前页浏览加购4个商品领爆竹').exists()) 
+    {
         log("当前页点击浏览或加购4个商品领爆竹");
         const productList = className('android.view.View').depth(15).indexInParent(4).clickable(true).find();
         var count = 0;
-        for (index = 0; index < productList.length; index++) {
-            if (count == 4) {
-                if (back()) {
+        for (index = 0; index < productList.length; index++) 
+        {
+            if (count == 4) 
+            {
+                if (back()) 
+                {
                     sleep(3000)
                     break;
                 }
             }
-            if (productList[index].click()) {
+            if (productList[index].click()) 
+            {
                 // 重置计时
                 JUDGE_TIME = 0;
                 log("浏览商品任务:正在浏览第" + (index + 1) + "个商品");
                 sleep(2000);
-                while (true) {
-                    if (text("购物车").exists() && back()) {
+                while (true) 
+                {
+                    if (text("购物车").exists() && back()) 
+                    {
                         count = count + 1;
+                        check_flag = 0;
                         sleep(2000);
-                        if (!text("购物车").exists()) {
+                        if (!text("购物车").exists()) 
+                        {
                             break;
                         }
                     }
@@ -415,41 +444,32 @@ for (let index = 0; index < allSelector.length; index++) {
             }
         }
     }
-}
-
-
-/**
-//做任务
- function execTask() {
-    while(true) {
-        if(text("浏览并关注8s可得8000爆竹").exists() || text("浏览8s可得8000爆竹").exists()) 
-        {
-            console.info("存在可浏览任务")
-        }
-        else
-        {
-            console.info("没有可浏览任务")
-            break
-        }
+    if(check_flag)
+    {
+        return true;
     }
 }
-execTask();
-*/
+
 
 /**
  * 自动判断程序是否卡顿，恢复方法
  * 判断依据：1.不在活动界面 2.停留某个界面长达30s
  * @returns
  */
- function recoverApp() {
-    if (!text("当前进度").exists() && JUDGE_TIME > 30) {
-        if (back()) {
+ function recoverApp() 
+ {
+    if (!text("当前进度").exists() && JUDGE_TIME > 30) 
+    {
+        if (back()) 
+        {
             // 计时器重置
             JUDGE_TIME = 0;
             console.warn("停留某个页面超过30s,自动返回，重置定时器。");
             return true;
         }
-    } else {
+    } 
+    else 
+    {
         return false;
     }
 }
@@ -458,9 +478,11 @@ execTask();
 /**
  * 返回
  */
-function viewAndFollow() {
+function viewAndFollow() 
+{
     trytime = 0;
-    while (!textContains("当前进度").exists() && trytime < 10) {
+    while (!textContains("当前进度").exists() && trytime < 10) 
+    {
         back();
         sleep(1000);
         trytime++;
